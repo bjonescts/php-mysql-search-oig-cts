8ef2b49ec459cea0e5c0393b0eaab3be6f76bb7f
bjonescts <70379785+bjonescts@users.noreply.github.com>
2021-06-02 10:30:35 -0400

    Update result.php

--- result.php
+++ result.php
@@ -1,24 +1,34 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "","shikin");
-//mysql_select_db("shikin", $conn);
+$conn = mysqli_connect("192.168.134.129", "adminer", "Teamcts123","oig");
+//mysql_select_db("oig", $conn);
 //search code
 //error_reporting(0);
 if($_REQUEST['submit']){
-$name = $_POST['name'];
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
 
-if(empty($name)){
-	$make = '<h4>You must type a word to search!</h4>';
+if(empty($lname)){
+        $make = '<h4>You must type a word to search!</h4>';
 }else{
-	$make = '<h4>No match found!</h4>';
-	$sele = "SELECT * FROM data WHERE name LIKE '%$name%'";
-	$result = mysqli_query($conn,$sele);
-	
-	if($row = mysqli_num_rows($result) > 0){
-		while($row = mysqli_fetch_assoc($result)){
-		echo '<h4> Id						: '.$row['id'];
-		echo '<br> name						: '.$row['name'];
-		echo '</h4>';
-	}
+        $make = '<h4>No match found!</h4>';
+        $sele = "SELECT * FROM oig WHERE lastname LIKE '%$lname%' AND firstname LIKE '%$fname%'";
+        $result = mysqli_query($conn,$sele);
+
+        if($row = mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                echo '<h4> Last name                                             : '.$row['lastname'];
+                echo '<br> First name                                            : '.$row['firstname'];
+                echo '<br> Business Name                                         : '.$row['busname'];
+                echo '<br> General Information                                   : '.$row['general'];
+                echo '<br> Specialty                                             : '.$row['specialty'];
+                echo '<br> Address                                               : '.$row['address'];
+                echo '<br> City                                                  : '.$row['city'];
+                echo '<br> ZIP                                                   : '.$row['zip'];
+                echo '<br> DOB                                                   : '.$row['dob'];
+                echo '<br> EXCL Type                                             : '.$row['excltype'];
+                echo '<br> EXCL Date                                             : '.$row['excldate'];
+                echo '</h4>';
+        }
 }else{
 echo'<h2> Search Result</h2>';
 
